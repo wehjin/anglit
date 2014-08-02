f17edafaaa2277ba44233d6c95dac482d8af5509
Jeffrey Yu <wehjin@gmail.com>
2014-08-02 08:42:33 -0700

    Refactor

--- app/src/main/java/com/rubyhuntersky/angleedit/app/MainActivity.java
+++ app/src/main/java/com/rubyhuntersky/angleedit/app/MainActivity.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -35,22 +31,6 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return item.getItemId() == R.id.action_settings || super.onOptionsItemSelected(item);
-    }
 
     /**
      * A placeholder fragment containing a simple view.
