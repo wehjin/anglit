16fd0adf34404fce6c32917279d8fe8927777743
Jeffrey Yu <wehjin@gmail.com>
2016-12-04 09:18:29 -0800

    Display multiple attributes

--- app/src/main/java/com/rubyhuntersky/angleedit/app/ElementCellViewHolder.kt
+++ app/src/main/java/com/rubyhuntersky/angleedit/app/ElementCellViewHolder.kt
@@ -1,8 +1,14 @@
 package com.rubyhuntersky.angleedit.app
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.cell_attribute.view.*
 import kotlinx.android.synthetic.main.cell_element.view.*
 import org.w3c.dom.Element
@@ -17,57 +23,40 @@ import org.w3c.dom.Node
 class ElementCellViewHolder(val itemView: View) {
     constructor(context: Context) : this(LayoutInflater.from(context).inflate(R.layout.cell_element, null, false))
 
+    private val NamedNodeMap.items: List<Node> get() = (0 until length).map { item(it) }
+    private val layoutInflater: LayoutInflater get() = LayoutInflater.from(itemView.context)
+    private val resources: Resources get() = itemView.context.resources
+    private val chipMargin: Int get() = resources.getDimensionPixelSize(R.dimen.chip_margin)
+    private fun Node.toUnattachedChipView(parent: ViewGroup): TextView {
+        val chipView = layoutInflater.inflate(R.layout.cell_attribute, parent, false) as TextView
+        chipView.text = textContent
+        return chipView
+    }
+
     fun bind(element: Element) {
         val detailText = element.firstTextString ?: ""
         if (detailText.isNotEmpty()) {
             itemView.textView.text = detailText
             itemView.secondaryTextView.text = element.tagName
             itemView.secondaryTextView.visibility = View.VISIBLE
+            itemView.chipsLayout.visibility = View.GONE
             itemView.chipView.visibility = View.GONE
         } else if (element.attributes.length > 0) {
             itemView.textView.text = "${element.tagName}\u2003"
             itemView.secondaryTextView.visibility = View.GONE
-            itemView.chipView.text = element.attributes.item(0).textContent
-            itemView.chipView.visibility = View.VISIBLE
+            itemView.chipsLayout.visibility = View.VISIBLE
+            itemView.chipsLayout.removeAllViews()
+            element.attributes.items.forEach {
+                val chipView = it.toUnattachedChipView(itemView.chipsLayout)
+                val layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, MATCH_PARENT)
+                layoutParams.marginEnd = chipMargin
+                layoutParams.marginStart = chipMargin
+                itemView.chipsLayout.addView(chipView, layoutParams)
+            }
         } else {
             itemView.textView.text = element.tagName
             itemView.secondaryTextView.visibility = View.GONE
-            itemView.chipView.visibility = View.GONE
-        }
-    }
-
-    private fun getDetailText(element: Element): String? {
-        val attributes = element.attributes
-        return if (attributes.length > 0) {
-            joinStrings(getAttributeDisplayStrings(attributes))
-        } else {
-            element.firstTextString
-        }
-    }
-
-    private fun joinStrings(strings: List<String>): String {
-        val builder = StringBuilder()
-        for (string in strings) {
-            if (builder.isNotEmpty()) {
-                builder.append(", ")
-            }
-            builder.append(string)
-        }
-        return builder.toString()
-    }
-
-    private fun getAttributeDisplayStrings(attributes: NamedNodeMap): List<String> {
-        return (0..attributes.length - 1).map {
-            getAttributeDisplayString(attributes.item(it))
-        }
-    }
-
-    private fun getAttributeDisplayString(attributeNode: Node): String {
-        val nodeValue = attributeNode.nodeValue
-        if (nodeValue == null || nodeValue == "") {
-            return attributeNode.nodeName
-        } else {
-            return nodeValue.trim { it <= ' ' }
+            itemView.chipsLayout.visibility = View.GONE
         }
     }
 }
